80e1575c0e40a68efb70d6e7e79537fa0c8f0473
Ion <Ion2000m@gmail.com>
2023-12-14 18:23:55 +0100

    first commit

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB!');
+}).catch(err => {
+    console.error('Error connecting to mongoDB', err);
+});
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server is running 3000!');
+});
