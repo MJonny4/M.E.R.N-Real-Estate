adec787350494a5b845bf93fd0c0dd53f913be53
Ion <Ion2000m@gmail.com>
2023-12-16 13:23:25 +0100

    Update error status codes in signin function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -44,14 +44,14 @@ export const signin = async (req, res, next) => {
 
     try {
         const validUser = await User.findOne({ email });
-        if (!validUser) return next(errorHandler(400, 'User not found!'));
+        if (!validUser) return next(errorHandler(404, 'User not found!'));
 
         const validPassword = bcryptjs.compareSync(
             password,
             validUser.password
         );
         if (!validPassword)
-            return next(errorHandler(400, 'Wrong credentials!'));
+            return next(errorHandler(401, 'Wrong credentials!'));
 
         const token = jwt.sign(
             {
