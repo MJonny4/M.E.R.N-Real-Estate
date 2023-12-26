34fb703cc988b1de91ad23f09d3e71a01c3ad205
Ion <Ion2000m@gmail.com>
2023-12-26 17:30:05 +0100

    Add profile page with user information and update tsconfig paths

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,3 +1,52 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+
 export default function Profile() {
-    return <div>Profile</div>;
+    const { currentUser } = useSelector((state: RootState) => state.user);
+
+    return (
+        <div className="p-3 max-w-lg mx-auto">
+            <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+            <form className="flex flex-col gap-4" action="">
+                <img
+                    src={currentUser?.avatar}
+                    alt={currentUser?.username}
+                    className="w-24 h-24 rounded-full object-cover cursor-pointer self-center mt-2"
+                />
+                <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Username"
+                    className="border p-3 rounded-lg"
+                />
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                    className="border p-3 rounded-lg"
+                />
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    className="border p-3 rounded-lg"
+                />
+                <button
+                    type="button"
+                    className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+                >
+                    Update
+                </button>
+            </form>
+            <div className="flex justify-between mt-5">
+                <span className="text-pink-900 cursor-pointer">
+                    Delete Account
+                </span>
+                <span className="text-red-700 cursor-pointer">Sign Out</span>
+            </div>
+        </div>
+    );
 }
