05adc4358c7e2c64fea1c2841e757f943225be7e
Ion <Ion2000m@gmail.com>
2023-12-27 13:15:37 +0100

    Add updateUser functionality and verify user token

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,42 @@
+import bcryptjs from 'bcryptjs';
+
+import { errorHandler } from '../utils/error.js';
+import User from '../models/user.model.js';
+
 export const test = (req, res) => {
     res.send('Hello World!');
 };
 
-export const updateUser = async (req, res) => {
-    
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id)
+        return next(errorHandler(401, 'Unauthorized to update this user'));
+
+    try {
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 12);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    avatar: req.body.avatar,
+                },
+            },
+            {
+                new: true,
+            }
+        );
+
+        const { password, ...user } = updatedUser._doc;
+        res.status(200).json({
+            success: true,
+            user,
+        });
+    } catch (err) {
+        return next(errorHandler(500, 'Error updating user'));
+    }
 };
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+import { errorHandler } from './error.js';
+
+dotenv.config();
+
+export const verifyToken = (req, res, next) => {
+    const access_token = req.cookies.access_token;
+
+    console.log(req);
+
+    if (!access_token) return next(errorHandler(401, 'Unauthorized'));
+
+    jwt.verify(access_token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return next(errorHandler(403, 'Forbidden'));
+        req.user = user; // or just user
+        return next();
+    });
+};
