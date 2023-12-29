8060783cdcf0d3a86be575e5c66df22eac1ff8c1
Ion <Ion2000m@gmail.com>
2023-12-30 00:39:52 +0100

    no

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -29,3 +29,4 @@ export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
