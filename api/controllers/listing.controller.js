955005ce0f1e011278d80f15077da4311f142da3
Ion <Ion2000m@gmail.com>
2024-01-01 13:57:19 +0100

    Add updateListing and getListing functions to listing.controller.js and update listing.route.js

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -39,3 +39,50 @@ export const deleteListing = async (req, res, next) => {
         next(error);
     }
 };
+
+export const updateListing = async (req, res, next) => {
+    try {
+        let listing = await Listing.findById(req.params.id);
+        if (!listing) {
+            return next(errorHandler(404, 'Listing not found'));
+        }
+
+        if (req.user.id !== listing.userRef.toString()) {
+            return next(
+                errorHandler(
+                    401,
+                    'You are not authorized to update this listing'
+                )
+            );
+        }
+
+        listing = await Listing.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: 'Listing updated successfully',
+            listing,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getListing = async (req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id);
+        if (!listing) {
+            return next(errorHandler(404, 'Listing not found'));
+        }
+
+        return res.status(200).json({
+            success: true,
+            listing,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,11 +5,15 @@ import { verifyToken } from '../utils/verifyUser.js';
 import {
     createListing,
     deleteListing,
+    updateListing,
+    getListing,
 } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.patch('/update/:id', verifyToken, updateListing);
+router.get('/get/:id', getListing);
 
 export default router;
